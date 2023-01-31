aed4cffd5ad9b166667d6574d2b8d9bd0d7d4a09
Braco <isovicramiz07@gmail.com>
2023-01-31 17:11:36 -0500

    Added html and tied it to js along with basic styling

--- script.js
+++ script.js
@@ -1,16 +1,69 @@
+const form = document.querySelector('form');
+const submitButton = document.querySelector(".submit")
 
-
-async function getWeather(loc) {
-    let key = 'a40f3a8337c485e713b0b666ad98b30b'
-    // let data = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=${key}`
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=${key}&units=imperial`)
+const getWeather = async (loc) => {
+    const apiKey = 'a40f3a8337c485e713b0b666ad98b30b'
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial`;
+    const response = await fetch(`${apiUrl}&q=${loc}`);
     const data = await response.json();
-    console.log(data) 
-    // .then(console.log(data))
+    console.log(data);
+    return data
 }
 
-// a40f3a8337c485e713b0b666ad98b30b
+const processData = (data) => {
+    return  {
+        location: data.name,
+        temp: data.main.temp,
+        weather: data.weather[0].description,
+    };
+};
 
-getWeather("Miami")
-getWeather("London")
-getWeather("Jacksonville")
+const DisplayData = (data) => {
+    const cityEl = document.querySelector(".city");
+    const temperatureEl = document.querySelector(".temperature");
+    const weatherEl = document.querySelector(".weather");
+
+    cityEl.textContent = data.location;
+    temperatureEl.textContent = `Temperature: ${Math.round(data.temp)}°F`;
+    weatherEl.textContent = `Weather: ${data.weather}`;
+}
+
+// getWeather("Miami")
+//     .then(data => {
+//         const processedData = processData(data);
+//         console.log(processedData);
+//     })
+//     .catch(error => console.error(error))
+
+
+submitButton.addEventListener("click", e => {
+    e.preventDefault();
+
+    const location = form.location.value
+
+    getWeather(location)
+    .then(data => {
+        const processedData = processData(data);
+        DisplayData(processedData)
+        console.log(processedData);
+    })
+    .catch(error => console.error(error))
+
+
+})
+
+
+// async function getWeather(loc) {
+//     let key = 'a40f3a8337c485e713b0b666ad98b30b'
+//     // let data = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=${key}`
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&APPID=${key}&units=imperial`)
+//     const data = await response.json();
+//     console.log(data) 
+//     // .then(console.log(data))
+// }
+
+// // a40f3a8337c485e713b0b666ad98b30b
+
+// getWeather("Miami")
+// getWeather("London")
+// getWeather("Jacksonville")
